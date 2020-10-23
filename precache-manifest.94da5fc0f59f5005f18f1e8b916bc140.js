e131530072623fb9d1f09a9b05fd194dc4f8938e
Marte Vinje <ge97hac@mytum.de>
2020-10-23 11:40:51 +0200

    Updates

---
+++ precache-manifest.94da5fc0f59f5005f18f1e8b916bc140.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1476329364ac3816c4bb979d93163e8c",
+    "url": "/webkurs2020-CICD-React/index.html"
+  },
+  {
+    "revision": "e77625ce66444bc7b13d",
+    "url": "/webkurs2020-CICD-React/static/css/2.0a5b19c0.chunk.css"
+  },
+  {
+    "revision": "e268a4fb22f59d217478",
+    "url": "/webkurs2020-CICD-React/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "e77625ce66444bc7b13d",
+    "url": "/webkurs2020-CICD-React/static/js/2.16ab8709.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/webkurs2020-CICD-React/static/js/2.16ab8709.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e268a4fb22f59d217478",
+    "url": "/webkurs2020-CICD-React/static/js/main.ccf5dde7.chunk.js"
+  },
+  {
+    "revision": "580e99ae8bc13b712167",
+    "url": "/webkurs2020-CICD-React/static/js/runtime-main.89e14c0b.js"
+  }
+]);
